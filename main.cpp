8717c7e62bb3b7a1a6c158623a6a643aed2f9947
SaNya-Griffin <117851071+SaNya-Griffin@users.noreply.github.com>
2022-11-09 11:10:40 -0600

    program 259 number 7

--- main.cpp
+++ main.cpp
@@ -1,1 +1,35 @@
-// write your code here
+// Sa'Nya Griffin J00964290 November 9
+#include <iostream>
+#include <cmath>
+using namespace std;
+
+int main() {
+ int b,a,c;
+  const double discriminant= pow(b,2)-4*a*c;
+
+  cout << "Enter a value:";
+  cin >> a;
+  cout << endl;
+
+  cout << "enter b value:";
+  cin >> b;
+  cout << endl;  
+
+  cout << "Enter c value:";
+  cin >> c;
+  cout << endl;
+cout << "Discriminant is:" << discriminant << endl;
+
+  if (discriminant> 0) 
+  {cout << "The equation has two real roots"<< endl;
+    }
+  if (discriminant < 0) 
+  {cout << "the equation has two complex roots"<< endl;}
+
+  if (discriminant == 0 )
+  {cout << "the equation has a single root"<< endl;}
+
+  return 0;
+  
+  
+}
